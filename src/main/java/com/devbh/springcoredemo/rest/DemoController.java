94fb0b1015b4373984ad0449f95cfd4a384c6c59
BurcuHaddad <burcuhaddad@gmail.com>
2024-01-08 20:30:14 +0300

    used qualifier for constructor dependency injection

--- src/main/java/com/devbh/springcoredemo/rest/DemoController.java
+++ src/main/java/com/devbh/springcoredemo/rest/DemoController.java
@@ -2,6 +2,7 @@ package com.devbh.springcoredemo.rest;
 
 import com.devbh.springcoredemo.common.Coach;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,9 +13,14 @@ public class DemoController {
 
     // define a constructor for dependency injection
     @Autowired
-    public DemoController(Coach theCoach) {
+    public DemoController(@Qualifier("cricketCoach") Coach theCoach) {
         myCoach = theCoach;
     }
+
+    /*@Autowired
+    public void setCoach(Coach theCoach) {
+        myCoach = theCoach;
+    }*/
 
     @GetMapping("/dailyworkout")
     public String getDailyWorkout() {
